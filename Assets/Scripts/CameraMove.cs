6fa3481d9eb32695c78415f0c775e93b8fa48eac
Narumik <contact.narumik@gmail.com>
2020-10-22 12:56:36 +0200

    Ready to build

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [Range(0.0f, 5.0f)]
+    public float vitesseAvance = 2;
+    [Range(0.0f, 360.0f)]
+    public float vitesseTourne = 20;
+
+    void Update()
+    {
+        transform.position += transform.forward * Time.deltaTime * vitesseAvance;
+        ///transform.rotation = Quaternion.AngleAxis(vitesseTourne * Time.deltaTime, Vector3.up) * transform.rotation;
+    }
+}

1982ccf4077eb2f90c7cd2860afccd4e3312479a
Narumik <contact.narumik@gmail.com>
2020-10-22 12:23:30 +0200

    Ajout patrick & ducks

---
+++ Assets/Canard.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canard : MonoBehaviour
+{
+    public float MinSoundDelay = 1;
+    public float RandomDelay = 5;
+    private float NextSoundDelay = 0;
+    private AudioSource aSource;
+
+    private void computeNextSoundDelay()
+    {
+        NextSoundDelay = MinSoundDelay + Random.Range(0, RandomDelay);
+    }
+
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        computeNextSoundDelay();
+    }
+
+    void Update()
+    {
+        NextSoundDelay -= Time.deltaTime;
+        if (NextSoundDelay <= 0)
+        {
+            aSource.Play();
+            computeNextSoundDelay();
+        }
+    }
+}
